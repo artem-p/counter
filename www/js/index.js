14c56b40b9535052b9b76525bc3f1eec4ce446a5
Artem Pugachev <artempugachev@iram.com>
2014-12-26 15:29:09 +0300

    Счетчик увеличивается по нажатию кнопки

--- www/js/index.js
+++ www/js/index.js
@@ -1,20 +1,34 @@
 //$("#counter").fitText();
-var counter = $("#counter");
-var count = counter.html();
-var  = count.length;
-setFontSize(nSymbol);             
+var m_counter = $("#counter");
+var m_count = 0;
+var increaseButton = $("#increaseButton");
 
+updateCounter();
 
-function setFontnSymbolSize(nSymbol) {
+increaseButton.click(increaseCounter);
+
+function updateCounter() {
+    var nSymbol = (m_count+"").length;
+    setFontSize(nSymbol);
+    m_counter.html(m_count);
+}
+
+function setFontSize(nSymbol) {
     //  Изменяем размер шрифта при изменении количества символов
     var symbolSize = ['8em', '8em', '8em', '6em', '6em', '5em', '4em', '3em', '2em', '1em'];
     //  Размер символов. Индекс массива - количество символов. С увеличением количества уменьшается размер
     var defaultSize = '1em';
 
     if(0<nSymbol && nSymbol <=symbolSize.length) {
-        counter.css("font-size", symbolSize[nSymbol-1]);
+        m_counter.css("font-size", symbolSize[nSymbol-1]);
     } else {
-        counter.css("font-size", defaultSize);
+        m_counter.css("font-size", defaultSize);
     }
 
+}
+
+function increaseCounter() {
+    //  Увеличиваем счетчик на 1
+    m_count++;
+    updateCounter();
 }
