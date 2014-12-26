f96703e82d49d5f7f2890108abec1e9471783256
Artem Pugachev <artempugachev@iram.com>
2014-12-26 15:16:29 +0300

    Функция изменения размера текста в зависимости от количества символов

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,20 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+//$("#counter").fitText();
+var counter = $("#counter");
+var count = counter.html();
+var  = count.length;
+setFontSize(nSymbol);             
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+function setFontnSymbolSize(nSymbol) {
+    //  Изменяем размер шрифта при изменении количества символов
+    var symbolSize = ['8em', '8em', '8em', '6em', '6em', '5em', '4em', '3em', '2em', '1em'];
+    //  Размер символов. Индекс массива - количество символов. С увеличением количества уменьшается размер
+    var defaultSize = '1em';
+
+    if(0<nSymbol && nSymbol <=symbolSize.length) {
+        counter.css("font-size", symbolSize[nSymbol-1]);
+    } else {
+        counter.css("font-size", defaultSize);
     }
-};
 
-app.initialize();
+}
